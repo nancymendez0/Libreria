d62cc2c7e7c842cf01fdebe637b43c5a32470156
eduar <eduar9807mail.com>
2025-08-11 18:35:31 -0600

    Agregando enum tipogenero

---
+++ src/com/PINACOMP/Sistemalibreria/model/enums/Ciencia.java
@@ -0,0 +1,4 @@
+package com.PINACOMP.Sistemalibreria.model.enums;
+
+public enum Ciencia {
+}
--- src/com/PINACOMP/Sistemalibreria/model/enums/TipoGenero.java
+++ src/com/PINACOMP/Sistemalibreria/model/enums/TipoGenero.java
@@ -1,4 +1,20 @@
 package com.PINACOMP.Sistemalibreria.model.enums;
 
 public enum TipoGenero {
+    TERROR("Terror"),
+    ROMANTICO("Romantico"),
+    FANTASIA("Fantasia"),
+    LITERARIO("Literario"),
+    CIENCIAFICCION("Ciencia ficción"),
+    NOVELA("Novela");
+    private final String genero;
+
+    TipoGenero(String genero) {
+        this.genero=genero;
+    }
+
+    @Override
+    public String toString() {
+        return this.genero;
+    }
 }
