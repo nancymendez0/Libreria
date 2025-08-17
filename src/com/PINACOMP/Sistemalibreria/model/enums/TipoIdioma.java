0b846e0afc9e8dbd86dfdf12aa33347c76bf8837
Nancy <cmendez782@yahoo.com>
2025-08-17 13:52:26 -0600

    Agregando método toString a enum TipoIdioma.java

--- src/com/PINACOMP/Sistemalibreria/model/enums/TipoIdioma.java
+++ src/com/PINACOMP/Sistemalibreria/model/enums/TipoIdioma.java
@@ -2,5 +2,12 @@ package com.PINACOMP.Sistemalibreria.model.enums;
 
 public enum TipoIdioma {
     ESPAÑOL,
-    INGLES;
+    FRANCES,
+    INGLES,
+    DESCONOCIDO;
+    @Override
+    public String toString() {
+        String nombre = name().toLowerCase();
+        //Separa el nombre , convirtiendo la primer letra a mayúscula ,despues une el resto del nombre
+        return nombre.substring(0, 1).toUpperCase() + nombre.substring(1);
 }
