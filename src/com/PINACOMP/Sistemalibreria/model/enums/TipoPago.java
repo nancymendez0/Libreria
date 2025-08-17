1a1e2b3062e44458c1a33eb65f1eb83b3a59d209
Nancy <cmendez782@yahoo.com>
2025-08-17 14:03:57 -0600

    Agregando método toString a enum TipoPago.java

--- src/com/PINACOMP/Sistemalibreria/model/enums/TipoPago.java
+++ src/com/PINACOMP/Sistemalibreria/model/enums/TipoPago.java
@@ -1,5 +1,17 @@
 package com.PINACOMP.Sistemalibreria.model.enums;
 
 public enum TipoPago {
-    EFECTIVO;
+    Efectivo,
+    Tarjeta_debito,
+    Tarjeta_credito,
+    Vale,
+    Transferencia,
+    Desconocido;
+    @Override
+    public String toString() {
+        String nombre = name().toLowerCase().replaceAll("_", " ");//devuelve el nombre exacto del valor enumerado tal como fue declarado .Convierte todo a minúsculas
+       //reemplaza los guioes bajos por espacios
+        //Da formato , conviertiendo la primer letra en mayúscula y une con el resto de la cadena
+        return nombre.substring(0, 1).toUpperCase() + nombre.substring(1);
+    }
 }
