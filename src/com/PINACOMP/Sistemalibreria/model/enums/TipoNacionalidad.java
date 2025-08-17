6739f5c7fa60af510d0b846b040df62a11498e92
Nancy <cmendez782@yahoo.com>
2025-08-17 13:59:32 -0600

    Agregando método toString a enum TipoNacionalidad.java

--- src/com/PINACOMP/Sistemalibreria/model/enums/TipoNacionalidad.java
+++ src/com/PINACOMP/Sistemalibreria/model/enums/TipoNacionalidad.java
@@ -1,5 +1,14 @@
 package com.PINACOMP.Sistemalibreria.model.enums;
 
 public enum TipoNacionalidad {
-    MEXICANA;
+    MEXICANA,
+    ARGENTINA,
+    ESPAÑOLA,
+    DESCONOCIDA;
+    @Override
+    public String toString() {
+        String nombre=name().toLowerCase();
+        //Da formato a nombre que contiene la nacionalidad , cambiando la primer letra a mayúscula
+        return nombre.substring(0, 1).toUpperCase() + nombre.substring(1);
+    }
 }
