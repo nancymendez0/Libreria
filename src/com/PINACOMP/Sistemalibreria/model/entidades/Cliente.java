82388f0fad63e0ac78256199217e2daa61e83a06
Nancy <cmendez782@yahoo.com>
2025-08-17 15:29:16 -0600

    Agregando clase Cliente.java , hereda atributos de clase Persona.class

---
+++ src/com/PINACOMP/Sistemalibreria/model/entidades/Cliente.java
@@ -0,0 +1,50 @@
+package com.PINACOMP.Sistemalibreria.model.entidades;
+
+import com.PINACOMP.Sistemalibreria.model.enums.TipoPago;
+import com.PINACOMP.Sistemalibreria.model.enums.TipoSexo;
+
+//Clase que representa un cliente en el sistema
+//Hereda atributos generales de la clase persona
+public class Cliente extends Persona{
+    //atributos privados clase CLIENTE
+    private int telefono;
+    private String direccion;
+    private TipoPago pago;
+    //constructor que inicializa todos los atributos del cliente incluyendo los heredados de persona
+    public Cliente(
+            int id,
+            String nombre,
+            String apellidoPaterno,
+            String correo,
+            int edad,
+            TipoSexo sexo,
+            TipoPago pago,
+            String direccion,
+            int telefono
+    ) {
+        super(nombre, apellidoPaterno, correo, edad, sexo, id);
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.pago = pago;
+    }
+    //Getters para devolver información usando enumeradores
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public TipoPago getPago() {
+        return pago;
+    }
+
+    //representacion textual del cliente , usada en el main
+    @Override
+    public String toString() {
+        return "Cliente # "+getId()+":"+getNombreCompleto()+"_"+direccion+"- Pago:"+pago;
+    }
+}
+
+
