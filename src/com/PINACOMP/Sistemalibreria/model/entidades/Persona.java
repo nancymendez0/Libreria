77bd2dc50bacbcec698f15fded3922f2096ae16e
Nancy <cmendez782@yahoo.com>
2025-08-17 15:35:56 -0600

    Agrege atributos y metodos a persona para poder heredar a cliente y autor

--- src/com/PINACOMP/Sistemalibreria/model/entidades/Persona.java
+++ src/com/PINACOMP/Sistemalibreria/model/entidades/Persona.java
@@ -1,7 +1,42 @@
 package com.PINACOMP.Sistemalibreria.model.entidades;
 
+import com.PINACOMP.Sistemalibreria.model.enums.TipoSexo;
+
 public class Persona {
-    String nombre;
-    String apellidoPaterno;
+    protected String nombre;
+    protected String apellidoPaterno;
+    protected String correo;
+    protected int edad;
+    protected TipoSexo sexo;
+    protected int id;//acceso desde subclases
+    //constructor
+    public Persona(String nombre, String apellidoPaterno, String correo, int edad, TipoSexo sexo, int id) {
+        this.id=id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.correo = correo;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellidoPaterno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public TipoSexo getSexo() {
+        return sexo;
+    }
+
 
 }
