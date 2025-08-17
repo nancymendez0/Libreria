5cdb0ac68d98d22510f59b558a323f659db7bd27
Nancy <cmendez782@yahoo.com>
2025-08-17 15:32:58 -0600

    Agregando clase Prestamo.java hereda de clase operacionesBiblioteca

---
+++ src/com/PINACOMP/Sistemalibreria/model/entidades/Prestamo.java
@@ -0,0 +1,24 @@
+package com.PINACOMP.Sistemalibreria.model.entidades;
+
+import java.time.LocalDate;
+
+public class Prestamo extends OperacionBiblioteca {
+    private int idCliente;
+    private String libro;
+    //constructor que inicializa los atributos del préstamo incluyendo los heredados
+    public Prestamo(int idOperacion, LocalDate fecha, int idCliente, String libro) {
+        super(idOperacion, fecha);
+        this.idCliente = idCliente;
+        this.libro = libro;
+    }
+    public void mostrarResumen() {
+        System.out.println(this.toString());
+    }
+    //sobre escribe toString para representar el préstamo como texto y usar en main para mostrar la información
+
+    @Override
+    public String toString() {
+        return "Préstamo #" + idOperacion + " | Cliente: " + idCliente + " | Libro: " + libro +
+                " | Fecha : " + fecha;
+    }
+}
