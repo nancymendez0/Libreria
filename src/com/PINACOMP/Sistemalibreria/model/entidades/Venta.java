09b3c2d42f36c900ae03902e9d609ab8c7e83970
Nancy <cmendez782@yahoo.com>
2025-09-01 12:08:33 -0600

    Agregando atributos y sobrecarga de metodos a clase Ventas

--- src/com/PINACOMP/Sistemalibreria/model/entidades/Venta.java
+++ src/com/PINACOMP/Sistemalibreria/model/entidades/Venta.java
@@ -1,20 +1,29 @@
 package com.PINACOMP.Sistemalibreria.model.entidades;
+import com.PINACOMP.Sistemalibreria.model.enums.TipoOperacion;
+
 import java.time.LocalDate;
 
 //clase ventas heredando de OperacionBiblioteca
-public class Venta extends OperacionBiblioteca {
+public class Venta {
+    //Nuevos atributos para implementar la imterfaz OperacionBliblioteca , ya no herefdan
+    private int idOperacion;
+    private LocalDate fecha;
+    private final TipoOperacion tipoOperacion= TipoOperacion.VENTA;
+    //-------Nancy
     private String libro;
     private double precio;
     //constructor publico de la clase ventas
     public Venta(int idOperacion, LocalDate fecha, String libro, double precio) {
-        super(idOperacion, fecha);
+        this.idOperacion = idOperacion;// no deberia heredar
+        //super(idOperacion, fecha);
         this.libro = libro;
         this.precio = precio;
     }
-    //método publico que imprime un resumen de la venta
+
     @Override
-    public void mostrarResumen() {
-        System.out.println("Venta #" + idOperacion + " | Libro: " + libro +
-                " | Fecha: " + fecha + " | Precio: $" + precio);
+    public TipoOperacion getTipoOperacion() {
+        return tipoOperacion;
     }
+
+
 }
